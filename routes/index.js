9dd0955bc3f9be7442d92be8ff73f472debf7e45
Andrei Voinea <andreivdev@protonmail.com>
2020-12-12 17:37:09 +0200

    prettier code format

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
   },
   {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
 ];
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: "Mini Messageboard", messages: messages })
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
-router.get('/new', function(req, res, next){
+router.get('/new', function (req, res, next) {
   res.render('form');
 });
 
-router.post("/new", function (req, res) {
+router.post('/new', function (req, res) {
   messages.push({
     text: req.body.message,
     user: req.body.author,
     added: new Date(),
   });
-  res.redirect("/");
+  res.redirect('/');
 });
 
 module.exports = router;
