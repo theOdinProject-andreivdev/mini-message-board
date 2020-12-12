809b3786b3639c23275a15b21a557f3bbc2d33f2
Andrei Voinea <andreivdev@protonmail.com>
2020-12-12 14:14:40 +0200

    add new messages page and button

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Mini Messageboard", messages: messages })
 });
 
+router.get('/new', function(req, res, next){
+  res.render('form');
+});
+
+router.post("/new", function (req, res) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
 module.exports = router;
